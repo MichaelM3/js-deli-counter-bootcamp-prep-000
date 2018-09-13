f6ca2921d26142e6e4fa7f8daff86891d7fb5546
Michael Muniz <michael.muniz94@gmail.com>
2018-09-13 20:30:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function currentLine(line) {
     return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`;
   }
   else if (line.length === 0) {
-    return 'The line is currently empty.'
+    return 'The line is currently empty.';
   }
 }
