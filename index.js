36eaef394bd27eedaf789db34743fe5f2e03d8ac
Michael Muniz <michael.muniz94@gmail.com>
2018-09-13 20:45:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
+
+
+
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine.shift([0])}.`;
